3131a411282f4d5079cc4a144d48f11ba24afddd
sankar10 <19ume062@lnmiit.ac.in>
2022-12-19 23:28:06 +0530

    Sell Coupon DataBase

--- app/src/main/java/com/example/messe/AdminSellCouponActivity.java
+++ app/src/main/java/com/example/messe/AdminSellCouponActivity.java
@@ -14,14 +14,21 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 public class AdminSellCouponActivity extends AppCompatActivity {
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -77,7 +84,28 @@ public class AdminSellCouponActivity extends AppCompatActivity {
             Toast.makeText(this, "Fields can not be null", Toast.LENGTH_SHORT).show();
             return;
         } else {
-            Toast.makeText(this, "Request Successfull", Toast.LENGTH_SHORT).show();
+            Map<String, Object> coupon = new HashMap<>();
+            coupon.put("name", name);
+            coupon.put("reason", reason);
+            coupon.put("meal", meal);
+            coupon.put("count", count);
+
+// Add a new document with a generated ID
+            db.collection("coupons")
+                    .add(coupon)
+                    .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                        @Override
+                        public void onSuccess(DocumentReference documentReference) {
+                            Log.d("TAG", "DocumentSnapshot added with ID: " + documentReference.getId());
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Log.w("TAG", "Error adding document", e);
+                        }
+                    });
+
             adminSellCouponNameEditText.setText("");
             adminSellCouponReasonEditText.setText("");
             adminSellCouponCount.setText("1");
